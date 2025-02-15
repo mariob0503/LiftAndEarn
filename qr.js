92f0680f1dcea9035e01daaf26c986a7d7c83199
mariob0503 <mario@simplix.solutions>
2025-02-15 16:16:46 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,9 +1,7 @@
 // qr.js
 function generateQRCode(containerId, url) {
   const container = document.getElementById(containerId);
-  // Clear any existing content in the container.
   container.innerHTML = "";
-  // Create the QR code.
   new QRCode(container, {
     text: url,
     width: 128,
